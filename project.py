dab24f52262f48340d9ef145bacad0cfb843a07a
Sahitya Kushwaha <83616018+sahitya1903@users.noreply.github.com>
2025-08-27 03:45:35 +0530

    Update comments

--- project.py
+++ project.py
@@ -2,7 +2,9 @@ import cv2
 import torch
 from twilio.rest import Client
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5l', trust_repo=True)
+from ultralytics import YOLO
+
+model = YOLO("yolo12x.pt")
 model.conf = 0.5
 
 
@@ -14,16 +16,15 @@ animal_keywords = [
 ]
 
 
-#Uncomment the code block below and enter your Twilio credentials
-'''
-
+#put original twilio credentials below
 account_sid = 'ABC'
-auth_token = 'xyz'
-twilio_number = '+1234'
+auth_token = '123'
+twilio_number = '+123'
 receiver_number = '+910000'
+
+
 client = Client(account_sid, auth_token)
 
-'''
 cap = cv2.VideoCapture(0)
 sent_labels = set()
 
@@ -32,16 +33,19 @@ while True:
     if not ret:
         break
 
-    results = model(frame)
-    detections = results.xyxy[0]
+    results = model(frame)  # results is a Results object
+    detections = results[0].boxes  # get detections for first image
 
-    for *box, conf, cls in detections:
-        label = model.names[int(cls)]
+    for box in detections:
+        x1, y1, x2, y2 = box.xyxy[0].cpu().numpy().astype(int)  # bounding box
+        conf = float(box.conf[0])  # confidence
+        cls = int(box.cls[0])      # class id
+        label = model.names[cls]
+
         if any(animal in label.lower() for animal in animal_keywords):
-            cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(
-                box[2]), int(box[3])), (0, 255, 0), 2)
-            cv2.putText(frame, label, (int(box[0]), int(
-                box[1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(frame, label, (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
             if label not in sent_labels:
                 print(f"{label} detected!")
@@ -55,6 +59,7 @@ while True:
                     sent_labels.add(label)
                 except Exception as e:
                     print("Error sending SMS:", e)
+
 
     cv2.imshow("Animal Detection", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
